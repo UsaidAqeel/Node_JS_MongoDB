4adfa5d2a5a394d7a1b224b79947bf0b270ca3cb
Usaid Aqeel <100064695+UsaidAqeel@users.noreply.github.com>
2022-12-07 00:03:57 +0500

    Server express js

---
+++ index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+// User Schema
+const Users = new Schema(
+    {
+        User_Name: String,
+        User_Comment: Array,
+        User_Todo: Array,
+        User_Gender: String
+    }
+)
+
+const User_Model = mongoose.model("AllUser", Users);
+export { User_Model }
